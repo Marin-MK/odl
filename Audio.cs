25bcc067f7d48670eb66354fd88aa35a177219a9
m3rein <m3rein@gmail.com>
2020-07-30 17:57:23 +0200

    New Audio interface

--- Audio.cs
+++ Audio.cs
@@ -1,147 +1,125 @@
-﻿using System;
-using System.Collections.Generic;
-using IrrKlang;
-using System.IO;
-using System.Threading;
+﻿#define WIN
+
+using System;
+using static Un4seen.Bass.Bass;
+using System.Runtime.InteropServices;
 
 namespace odl
 {
+    public delegate void SoundCallback(long Position);
+    public delegate void SlideCallback(int SlideType);
+
     public static class Audio
     {
-        static bool Stopped = false;
-        public static ISoundEngine SoundEngine;
+        public static Sound BGM;
 
-        public static List<Sound> Sounds = new List<Sound>();
+        public static bool UsingBassFX = true;
+        static int FXPlugin;
 
-        private static int __system_volume__ = 100;
-        public static int SystemVolume { get { return __system_volume__; } set { __system_volume__ = value; if (SoundEngine != null) SoundEngine.SoundVolume = value / 100f; } }
+        #region Linux
+        #if LINUX
+        [DllImport("libdl.so")]
+        static extern IntPtr dlopen(string filename, int flags);
 
-        public static void Start()
+        [DllImport("libdl.so")]
+        static extern string dlerror();
+        #endif
+        #endregion
+
+        public static void Start(bool UseBassFX = true)
         {
-            SoundEngine = new ISoundEngine();
-            SoundEngine.SoundVolume = SystemVolume / 100f;
-            Thread thread = null;
-            thread = new Thread(delegate ()
+            Audio.UsingBassFX = UseBassFX;
+            #region Load bass & bass_fx
+            #if LINUX
+            if (dlopen("./libbass.so", 0x101) == IntPtr.Zero)
+                throw new Exception($"Failed to load libbass.so\nError: {dlerror()}");
+            if (UseBassFX)
             {
-                while (!Stopped) Update();
-            });
-            thread.Start();
+                if (if (dlopen("./libbass_fx.so", 0x101) == IntPtr.Zero)
+                    throw new Exception($"Failed to load libbass_fx.so\nError: {dlerror()}");
+                FXPlugin = BASS_PluginLoad("libbass_fx.so");
+            }
+            #else
+            if (UseBassFX) FXPlugin = BASS_PluginLoad("bass_fx.dll");
+            #endif
+            #endregion
+            BASS_Init(-1, 44100, 0, IntPtr.Zero);
         }
 
         public static void Stop()
         {
-            SoundEngine.StopAllSounds();
-            SoundEngine.Dispose();
-            SoundEngine = null;
-            Stopped = true;
+            BASS_Free();
+            if (FXPlugin != 0) BASS_PluginFree(FXPlugin);
         }
 
-        public static void Play(string Filename, int Volume = 100)
+        public static void BGMPlay(string Filename, int Volume = 100, int Pitch = 0)
         {
-            Play(new Sound(Filename, Volume));
+            BGMPlay(new Sound(Filename, Volume, Pitch));
+        }
+        public static void BGMPlay(Sound Sound)
+        {
+            if (BGM != null)
+            {
+                BGM.FadeOut(BGM.SampleRate / 4, delegate (int SlideType)
+                {
+                    if (SlideType == 2)
+                    {
+                        BGM.Stop();
+                        BGM = Sound;
+                        BGM.Looping = true;
+                        BGM.AddEndCallback(delegate (long Position) { BGM = null; });
+                        BGM.FadeIn(BGM.SampleRate / 4);
+                        BGM.Play();
+                    }
+                });
+            }
+            else
+            {
+                BGM = Sound;
+                BGM.Looping = true;
+                BGM.AddEndCallback(delegate (long Position) { BGM = null; });
+                BGM.Play();
+            }
         }
 
+        public static void MEPlay(string Filename, int Volume = 100, int Pitch = 0)
+        {
+            MEPlay(new Sound(Filename, Volume, Pitch));
+            
+        }
+        public static void MEPlay(Sound Sound)
+        {
+            BGM.FadeOut(BGM.SampleRate / 4, delegate (int SlideType)
+            {
+                if (SlideType == 2)
+                {
+                    BGM.Pause();
+                    Sound.Play();
+                    Sound.AddEndCallback(delegate (long _)
+                    {
+                        BGM.FadeIn(BGM.SampleRate / 4);
+                        BGM.Play();
+                    });
+                }
+            });
+        }
+
+        public static void SEPlay(string Filename, int Volume = 100, int Pitch = 0)
+        {
+            SEPlay(new Sound(Filename, Volume, Pitch));
+        }
+        public static void SEPlay(Sound Sound)
+        {
+            Sound.Play();
+        }
+
+        public static void Play(string Filename, int Volume = 100, int Pitch = 0)
+        {
+            Play(new Sound(Filename, Volume, Pitch));
+        }
         public static void Play(Sound Sound)
         {
-            Sound.GuardDisposed();
-            string filename = Sound.Filename;
-            if (!File.Exists(filename))
-            {
-                if (File.Exists(filename + ".wav")) filename += ".wav";
-                else if (File.Exists(filename + ".ogg")) filename += ".ogg";
-                else if (File.Exists(filename + ".mp3")) filename += ".mp3";
-                else if (File.Exists(filename + ".flac")) filename += ".flac";
-                else if (File.Exists(filename + ".mod")) filename += ".mod";
-                else if (File.Exists(filename + ".it")) filename += ".it";
-                else if (File.Exists(filename + ".s3d")) filename += ".s3d";
-                else if (File.Exists(filename + ".xm")) filename += ".xm";
-            }
-            if (Sound.__sound__ != null)
-            {
-                Sound.__sound__.Stop();
-                Sound.__sound__.Dispose();
-            }
-            Sound.__sound__ = SoundEngine.Play2D(filename, Sound.Loop);
-            if (Sound.__sound__ == null)
-            {
-                throw new Exception($"File not found - {Sound.Filename}");
-            }
-            Sound.Volume = Sound.__volume__;
-            Sound.Position = Sound.__position__;
-            Sound.Pan = Sound.__pan__;
-            Sound.__sound__.setSoundStopEventReceiver(new InternalStopReceiver(Sound));
-            Sounds.Add(Sound);
-        }
-
-        public static void Update()
-        {
-            for (int i = 0; i < Sounds.Count; i++)
-            {
-                Sound Sound = Sounds[i];
-                if (Sound == null)
-                {
-                    Sounds.RemoveAt(i);
-                    i--;
-                    continue;
-                }
-                if (Sound.__sound__ == null || Sound.__sound__.Finished) continue;
-                if (Sound.Loop && (Sound.LoopTimes == -1 || Sound.TimesLooped < Sound.LoopTimes))
-                {
-                    if (Sound.__sound__.PlayPosition < Sound.__oldpos__ ||
-                        Sound.LoopEndPosition != 0 && Sound.__sound__.PlayPosition >= Sound.LoopEndPosition)
-                    {
-                        // Finished playing or reached loop end point
-                        Sound.OnLoop?.Invoke(new BaseEventArgs());
-                        Sound.__sound__.PlayPosition = Sound.LoopStartPosition;
-                        Sound.TimesLooped++;
-                        if (Sound.LoopTimes != -1 && Sound.TimesLooped >= Sound.LoopTimes)
-                        {
-                            Sound.__sound__.Looped = false;
-                        }
-                    }
-                    Sound.__oldpos__ = Sound.__sound__.PlayPosition;
-                }
-                if (Sound.FadeInLength != 0 && Sound.Position < Sound.FadeInLength && Sound.TimesLooped == 0)
-                {
-                    float fraction = (float) Sound.Position / Sound.FadeInLength;
-                    Sound.__sound__.Volume = Sound.__volume__ / 100f * fraction;
-                    Sound.__fade_in__ = true;
-                }
-                else if (Sound.__fade_in__)
-                {
-                    Sound.__fade_in__ = false;
-                    Sound.__sound__.Volume = Sound.__volume__ / 100f;
-                    Sound.OnFadedIn?.Invoke(new BaseEventArgs());
-                }
-                if (Sound.FadeOutLength != 0 && Sound.Position >= Sound.__sound__.PlayLength - Sound.FadeOutLength &&
-                    (Sound.LoopTimes != -1 && Sound.TimesLooped == Sound.LoopTimes || Sound.LoopTimes == -1 && !Sound.Loop))
-                {
-                    float fraction = (float) (Sound.__sound__.PlayLength - Sound.Position) / Sound.FadeOutLength;
-                    Sound.__sound__.Volume = Sound.__volume__ / 100f * fraction;
-                    if (!Sound.__fade_out__)
-                    {
-                        Sound.OnFadingOut?.Invoke(new BaseEventArgs());
-                        Sound.__fade_out__ = true;
-                        Sound.__sound__.Volume = 0;
-                    }
-                }
-                Sound.OnUpdate?.Invoke(new BaseEventArgs());
-            }
-        }
-
-        public class InternalStopReceiver : ISoundStopEventReceiver
-        {
-            public Sound Sound;
-
-            public InternalStopReceiver(Sound Sound)
-            {
-                this.Sound = Sound;
-            }
-
-            public void OnSoundStopped(ISound sound, StopEventCause reason, object userData)
-            {
-                this.Sound.OnStopped?.Invoke(new BaseEventArgs());
-            }
+            Sound.Play();
         }
     }
 }
--- Sound.cs
+++ Sound.cs
@@ -1,90 +1,348 @@
-﻿using IrrKlang;
-using System;
+﻿using System;
+using Un4seen.Bass;
+using static Un4seen.Bass.Bass;
+using static Un4seen.Bass.AddOn.Fx.BassFx;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace odl
 {
     public class Sound
     {
-        public delegate void BaseEvent(BaseEventArgs e);
+        protected SoundCallback LoopCallback;
 
-        // General
-        public string Filename;
-        public int __volume__ = 100;
-        public int Volume { get { return __sound__ == null ? __volume__ : (int) Math.Round(__sound__.Volume * 100); } set { if (__sound__ != null) __sound__.Volume = value / 100f; __volume__ = value; } }
-        public uint __position__ = 0;
-        public uint Position { get { return __sound__ == null ? __position__ : __sound__.PlayPosition; } set { if (__sound__ != null) __sound__.PlayPosition = value; __position__ = value; __oldpos__ = value; } }
-        public int __pan__ = 0;
-        public int Pan { get { return __sound__ == null ? __pan__ : (int) Math.Round(__sound__.Pan * -100); } set { if (__sound__ != null) __sound__.Pan = value / -100f; __pan__ = value; } }
-        public bool Paused { get { return __sound__ == null ? false : __sound__.Paused; } }
-        public bool Playing { get { return __sound__ != null && !Paused && !__sound__.Finished; } }
-        public bool Disposed = false;
+        protected List<SYNCPROC> SYNCPROC_CACHE = new List<SYNCPROC>();
 
-        // Looping
-        public bool Loop = false;
-        public int LoopTimes = -1;
-        public uint LoopStartPosition = 0;
-        public uint LoopEndPosition = 0;
-        public int TimesLooped = 0;
+        protected int Stream;
+        protected int LoopEndCallback;
+        protected int FadeOutCallback;
 
-        // Fading
-        public uint FadeInLength = 0;
-        public uint FadeOutLength = 0;
+        protected int OriginalVolume = 0;
 
-        // Events
-        public BaseEvent OnFadedIn;
-        public BaseEvent OnFadingOut;
-        public BaseEvent OnLoop;
-        public BaseEvent OnStopped;
-        public BaseEvent OnUpdate;
-
-        // Internal
-        public bool __fade_in__ = false;
-        public bool __fade_out__ = false;
-        public uint __oldpos__ = 0;
-        public ISound __sound__;
-
-        public Sound(string Filename, int Volume = 100)
+        public string Filename { get; }
+        protected int _Volume = 100;
+        public int Volume
         {
-            this.Filename = Filename;
-            this.Volume = Volume;
+            get
+            {
+                return _Volume;
+            }
+            set
+            {
+                if (value != _Volume && Stream != 0)
+                    BASS_ChannelSetAttribute(Stream, BASSAttribute.BASS_ATTRIB_VOL, value / 100f);
+                _Volume = value;
+                OriginalVolume = value;
+            }
+        }
+        protected double _Pitch = 0;
+        public double Pitch
+        {
+            get
+            {
+                return _Pitch;
+            }
+            set
+            {
+                if (value != 0 && !Audio.UsingBassFX) throw new Exception("Cannot change Pitch when odl Audio is initialized with UsingBassFX set to false.");
+                if (value != _Pitch && Stream != 0)
+                    BASS_ChannelSetAttribute(Stream, BASSAttribute.BASS_ATTRIB_TEMPO_PITCH, (float) value);
+                _Pitch = value;
+            }
+        }
+        protected int _SampleRate = 0;
+        public int SampleRate
+        {
+            get
+            {
+                return _SampleRate;
+            }
+            set
+            {
+                if (value != _SampleRate && Stream != 0)
+                    BASS_ChannelSetAttribute(Stream, BASSAttribute.BASS_ATTRIB_FREQ, value);
+                _SampleRate = value;
+            }
+        }
+        protected double _Pan = 0;
+        public double Pan
+        {
+            get
+            {
+                return _Pan;
+            }
+            set
+            {
+                if (value != _Pan && Stream != 0)
+                    BASS_ChannelSetAttribute(Stream, BASSAttribute.BASS_ATTRIB_PAN, (float) value);
+                _Pan = value;
+            }
+        }
+        public long Position
+        {
+            get
+            {
+                return BASS_ChannelGetPosition(this.Stream) / 4;
+            }
+            set
+            {
+                BASS_ChannelSetPosition(this.Stream, value * 4);
+            }
         }
 
-        public void Play()
+        protected bool _Looping = false;
+        public bool Looping
         {
-            GuardDisposed();
-            Audio.Play(this);
+            get
+            {
+                return _Looping;
+            }
+            set
+            {
+                _Looping = value;
+                ReconfigureLooping();
+            }
+        }
+        protected long _LoopStart = 0;
+        public long LoopStart
+        {
+            get
+            {
+                return _LoopStart;
+            }
+            set
+            {
+                _LoopStart = value;
+                ReconfigureLooping();
+            }
+        }
+        protected long _LoopEnd = 0;
+        public long LoopEnd
+        {
+            get
+            {
+                return _LoopEnd;
+            }
+            set
+            {
+                _LoopEnd = value;
+                ReconfigureLooping();
+            }
+        }
+
+        protected long _FadeInLength = 0;
+        public long FadeInLength
+        {
+            get
+            {
+                return _FadeInLength;
+            }
+            set
+            {
+                if (value != _FadeInLength && Position == 0)
+                {
+                    this.Volume = 0;
+                    this.SlideVolume(100, value);
+                }
+                _FadeInLength = value;
+            }
+        }
+        protected long _FadeOutLength = 0;
+        public long FadeOutLength
+        {
+            get
+            {
+                return _FadeOutLength;
+            }
+            set
+            {
+                if (value != _FadeOutLength)
+                {
+                    if (FadeOutCallback != 0) RemoveCallback(FadeOutCallback);
+                    FadeOutCallback = AddPositionCallback(Length - value, delegate (long Position)
+                    {
+                        SlideVolume(0, FadeOutLength);
+                    });
+                }
+                _FadeOutLength = value;
+            }
+        }
+
+        public bool Playing
+        {
+            get
+            {
+                return BASS_ChannelIsActive(this.Stream) == BASSActive.BASS_ACTIVE_PLAYING;
+            }
+        }
+
+        public long Length { get; protected set; }
+
+        public Sound(string Filename, int Volume = 100, double Pitch = 0)
+        {
+            if (Pitch != 0 && !Audio.UsingBassFX) throw new Exception("Cannot change Pitch when odl Audio is initialized with UsingBassFX set to false.");
+            string OriginalFilename = Filename;
+            if (!File.Exists(Filename))
+            {
+                if (File.Exists(Filename + ".ogg")) Filename += ".ogg";
+                else if (File.Exists(Filename + ".wav")) Filename += ".wav";
+                else if (File.Exists(Filename + ".mp3")) Filename += ".mp3";
+            }
+            if (Audio.UsingBassFX)
+            {
+                int BaseStream = BASS_StreamCreateFile(Filename, 0, 0, BASSFlag.BASS_STREAM_DECODE);
+                this.Stream = BASS_FX_TempoCreate(BaseStream, BASSFlag.BASS_STREAM_AUTOFREE | BASSFlag.BASS_FX_FREESOURCE);
+            }
+            else
+            {
+                this.Stream = BASS_StreamCreateFile(Filename, 0, 0, 0);
+            }
+            float sr = 0;
+            BASS_ChannelGetAttribute(this.Stream, BASSAttribute.BASS_ATTRIB_FREQ, ref sr);
+            this.Length = BASS_ChannelGetLength(this.Stream) / 4;
+            _SampleRate = (int) sr;
+            this.Filename = OriginalFilename;
+            this.Volume = Volume;
+            this.Pitch = Pitch;
+        }
+
+        public int AddPositionCallback(long Sample, SoundCallback Callback)
+        {
+            SYNCPROC proc = delegate (int Handle, int Channel, int Data, IntPtr User) { Callback(Sample); };
+            SYNCPROC_CACHE.Add(proc);
+            return BASS_ChannelSetSync(this.Stream, BASSSync.BASS_SYNC_POS, Math.Max(0, Sample * 4 - 18000), proc, IntPtr.Zero);
+        }
+
+        public int AddEndCallback(SoundCallback Callback)
+        {
+            SYNCPROC proc = delegate (int Handle, int Channel, int Data, IntPtr User) { Callback(this.Length); };
+            SYNCPROC_CACHE.Add(proc);
+            return BASS_ChannelSetSync(this.Stream, BASSSync.BASS_SYNC_END | BASSSync.BASS_SYNC_MIXTIME, 0, proc, IntPtr.Zero);
+        }
+
+        public int AddSlideCallback(SlideCallback Callback)
+        {
+            SYNCPROC proc = delegate (int Handle, int Channel, int Data, IntPtr User)
+            {
+                Callback(Data);
+            };
+            SYNCPROC_CACHE.Add(proc);
+            return BASS_ChannelSetSync(this.Stream, BASSSync.BASS_SYNC_SLIDE | BASSSync.BASS_SYNC_MIXTIME, 0, proc, IntPtr.Zero);
+        }
+
+        public void AddLoopCallback(SoundCallback Callback)
+        {
+            LoopCallback += Callback;
+        }
+
+        public void RemoveCallback(int Handle)
+        {
+            BASS_ChannelRemoveSync(this.Stream, Handle);
+        }
+
+        protected void ReconfigureLooping()
+        {
+            BASS_ChannelFlags(this.Stream, 0, BASSFlag.BASS_SAMPLE_LOOP);
+            if (LoopEndCallback != 0) RemoveCallback(LoopEndCallback);
+            if (LoopStart != 0 && LoopEnd == 0)
+            {
+                LoopEndCallback = AddEndCallback(delegate (long Position) { this.Position = LoopStart; LoopCallback?.Invoke(Position); });
+            }
+            else if (LoopStart != LoopEnd)
+            {
+                LoopEndCallback = AddPositionCallback(LoopEnd, delegate (long Position) { this.Position = LoopStart; LoopCallback?.Invoke(Position); });
+            }
+            else if (Looping)
+            {
+                LoopEndCallback = AddEndCallback(delegate (long Position) { LoopCallback?.Invoke(Position); });
+                BASS_ChannelFlags(this.Stream, BASSFlag.BASS_SAMPLE_LOOP, BASSFlag.BASS_SAMPLE_LOOP);
+            }
+        }
+
+        public void SlideVolume(int Volume, long Time)
+        {
+            _Volume = Volume;
+            BASS_ChannelSlideAttribute(this.Stream, BASSAttribute.BASS_ATTRIB_VOL, Volume / 100f, (int) Math.Round((double) Time / SampleRate * 1000));
+        }
+
+        public void SlidePan(double Pan, long Time)
+        {
+            _Pan = Pan;
+            BASS_ChannelSlideAttribute(this.Stream, BASSAttribute.BASS_ATTRIB_PAN, (float) Pan, (int) Math.Round((double) Time / SampleRate * 1000));
+        }
+
+        public void SlidePitch(double Pitch, long Time)
+        {
+            _Pitch = Pitch;
+            BASS_ChannelSlideAttribute(this.Stream, BASSAttribute.BASS_ATTRIB_TEMPO_PITCH, (float) Pitch, (int) Math.Round((double) Time / SampleRate * 1000));
+        }
+
+        public void SlideSampleRate(int SampleRate, long Time)
+        {
+            _Pitch = Pitch;
+            BASS_ChannelSlideAttribute(this.Stream, BASSAttribute.BASS_ATTRIB_FREQ, SampleRate, (int) Math.Round((double) Time / SampleRate * 1000));
+        }
+
+        public void FadeOut(long Time)
+        {
+            SlideVolume(0, Time);
+        }
+        public void FadeOut(long Time, SlideCallback Callback)
+        {
+            SlideVolume(0, Time);
+            int Handle = 0;
+            Handle = AddSlideCallback(delegate (int SlideType)
+            {
+                Callback(SlideType);
+                RemoveCallback(Handle);
+            });
+        }
+
+        public void FadeIn(long Time)
+        {
+            SlideVolume(OriginalVolume, Time);
+        }
+        public void FadeIn(long Time, SlideCallback Callback)
+        {
+            SlideVolume(OriginalVolume, Time);
+            int Handle = 0;
+            Handle = AddSlideCallback(delegate (int SlideType)
+            {
+                Callback(SlideType);
+                RemoveCallback(Handle);
+            });
         }
 
         public void Pause()
         {
-            GuardDisposed();
-            __sound__.Paused = true;
+            BASS_ChannelPause(this.Stream);
         }
 
         public void Resume()
         {
-            GuardDisposed();
-            __sound__.Paused = false;
+            BASS_ChannelPlay(this.Stream, false);
+        }
+
+        public void Play()
+        {
+            if (BASS_ChannelIsActive(this.Stream) == BASSActive.BASS_ACTIVE_PAUSED)
+            {
+                BASS_ChannelPlay(this.Stream, false);
+            }
+            else
+            {
+                BASS_ChannelPlay(this.Stream, true);
+            }
+        }
+
+        public void Start()
+        {
+            BASS_ChannelPlay(this.Stream, true);
         }
 
         public void Stop()
         {
-            GuardDisposed();
-            __sound__.Stop();
-        }
-
-        public void Dispose()
-        {
-            if (__sound__ != null) __sound__.Dispose();
-            __sound__ = null;
-            this.Disposed = true;
-        }
-
-        public void GuardDisposed()
-        {
-            if (this.Disposed) throw new Exception($"Sound already disposed.");
+            BASS_ChannelStop(this.Stream);
         }
     }
 }
