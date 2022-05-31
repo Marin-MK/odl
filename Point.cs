6ace7146d26bc42f88f70f7304fbf4c232ba840a
Marijn Herrebout <m.k.herrebout@student.tudelft.nl>
2022-05-31 19:06:31 +0200

    Utility methods for Point

--- Point.cs
+++ Point.cs
@@ -27,4 +27,24 @@ public class Point : IDisposable
     {
         return $"(Point: {this.X},{this.Y})";
     }
+
+    public static Point operator +(Point p1, Point p2)
+    {
+        return new Point(p1.X + p2.X, p1.Y + p2.Y);
+    }
+
+    public static Point operator -(Point p1, Point p2)
+    {
+        return new Point(p1.X - p2.X, p1.Y - p2.Y);
+    }
+
+    public Point Abs()
+    {
+        return new Point(Math.Abs(X), Math.Abs(Y));
+    }
+
+    public Size ToSize()
+    {
+        return new Size(this.X, this.Y);
+    }
 }
